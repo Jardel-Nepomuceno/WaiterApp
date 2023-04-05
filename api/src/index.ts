8067c3679f70e32ae390c1276443851a8aa49445
jardel <jardelsantos15@gmail.com>
2023-04-05 14:01:43 -0300

    Configuração Docker

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,10 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
-const app = express();
 
-const port = 3001;
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+mongoose.connect('mongodb://localhost:8080')
+  .then(() => {
+    const app = express();
+    const port = 3001;
+    app.listen(port, () => {
+      console.log(`Server is running on http://localhost:${port}`);
 
-});
+    });
+  })
+  .catch(() => console.log('Erro ao conectar no mongodb'));
+
+
