159be14703c4f624454589e2e745131c333d8484
jardel <jardelsantos15@gmail.com>
2023-04-11 21:46:47 -0300

    Criação dos componentes Header, Orders e Boards dos pedidos + configuração do projeto.

---
+++ front/src/components/Orders/index.tsx
@@ -0,0 +1,19 @@
+import { OrdersBoard } from '../OdersBoard';
+import { Container } from './styles';
+
+
+export function Orders() {
+  return (
+    <Container>
+      <OrdersBoard
+        icon="🕛"
+        title="Fila de espera" orders={[]} />
+      <OrdersBoard
+        icon="👨‍🍳"
+        title="Em preparação" orders={[]} />
+      <OrdersBoard
+        icon="✅"
+        title="Pronto!" orders={[]} />
+    </Container>
+  );
+}
